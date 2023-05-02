d9fe9c0865770cb536207638b89fbcb2de9d94d7
Klaudia Wojcieska <klaudiawojcieska@Klaudias-MacBook-Air.local>
2023-05-02 15:32:25 +0100

    models and views configured

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+
+class ArticleList(generics.ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    
+class ArticleDetail(generics.RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
