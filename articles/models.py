f94ac6b6c88301a6e28f50eb32267beeaee11e65
Klaudia Wojcieska <klaudiawojcieska@Klaudias-MacBook-Air.local>
2023-05-04 23:20:43 +0100

    api added

--- articles/models.py
+++ articles/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
+    CATEGORIES = (
+        ('1', 'Eco'),
+        ('2', 'Sustainability'),
+    )
+    category = models.CharField(max_length=250, choices=CATEGORIES)
     title = models.CharField(max_length=250)
+    img_name = models.CharField(max_length=250)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- articles/serializers.py
+++ articles/serializers.py
@@ -3,5 +3,5 @@ from rest_framework import serializers
 from . import models
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'title', 'content', 'created_at', 'updated_at',)
+        fields = ('id', 'img_name','title', 'category', 'content', 'created_at', 'updated_at',)
         model = models.Article
